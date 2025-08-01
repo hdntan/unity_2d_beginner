e76125248dc0a7b38e758aac559aa73a4a572fa5
hdntan <hdntan0903@gmail.com>
2025-08-01 23:03:57 +0700

    feat player move using input action

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+//         QualitySettings.vSyncCount = 0;
+// Application.targetFrameRate = 10;
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+     Debug.Log(move);
+     Vector2 position = (Vector2)transform.position + move * 3f * Time.deltaTime;
+     transform.position = position;
     }
+
+
 }
+
