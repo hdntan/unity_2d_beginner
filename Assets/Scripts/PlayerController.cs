9d3a6a9f6b73a5d938c4a5151ce6a8736c0b35a8
hdntan <hdntan0903@gmail.com>
2025-08-02 23:29:30 +0700

    feat add collider tilemap  and rigibody player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,31 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    [SerializeField] protected Rigidbody2D rigidbody2d;
+
+    [SerializeField] protected Vector2 move;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-//         QualitySettings.vSyncCount = 0;
-// Application.targetFrameRate = 10;
+        //         QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-     Debug.Log(move);
-     Vector2 position = (Vector2)transform.position + move * 3f * Time.deltaTime;
-     transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+
+    }
+    
+    void FixedUpdate()
+    {
+          Vector2 position = (Vector2)transform.position + move * 3f * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
 
 
